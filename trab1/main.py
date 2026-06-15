01fc877684cf02e699bb100d712d17396ed487ea
RafaelFFAumo <rafael@aumo.ai>
2026-06-15 20:07:23 -0300

    trabalho feito, estou apenas ajeitando no collab

--- trab1/main.py
+++ trab1/main.py
@@ -10,23 +10,21 @@ from pymoo.algorithms.soo.nonconvex.es import ES
 from pymoo.optimize import minimize
 from pymoo.core.population import Population
 from pymoo.core.problem import Problem
-from pymoo.termination.max_gen import MaximumGenerationTermination
-from pymoo.termination.fmin import MinimumFunctionValueTermination
-from pymoo.termination.collection import TerminationCollection
+from pymoo.core.algorithm import Algorithm
 
 from dataclasses import dataclass
-from functools import partial
 
 from pymoo.algorithms.soo.nonconvex.de import DE
 from pymoo.algorithms.soo.nonconvex.ga import FitnessSurvival
 from pymoo.core.replacement import ImprovementReplacement
-from pymoo.core.sampling import Sampling
+
+from time import time
 
 
 iris = load_iris()
 x_iris = iris.data       # características
 y_iris = iris.target     # rótulos (apenas para avaliação)
-iris_min_3_cluster = 78.8514
+# iris_min_3_cluster = 78.8514
 
 breast = load_breast_cancer()
 x_breast = breast.data
@@ -35,8 +33,6 @@ y_breast = breast.target
 wine = load_wine()
 x_wine = wine.data
 y_wine = wine.target
-
-DE_population_size = 30
 
 @dataclass
 class FitnessHistory:
@@ -113,12 +109,215 @@ class CustomDE_Kmeans(DE):
         )
 
         # get the indices where each offspring is originating from
-        I = infills.get("index")
+        pop_i = infills.get("index")
         # replace the individuals with the corresponding parents from the mating
-        self.pop[I] = ImprovementReplacement().do(self.problem, self.pop[I], infills)
+        self.pop[pop_i] = ImprovementReplacement().do(self.problem, self.pop[pop_i], infills)
 
         # update the information regarding the current population
         FitnessSurvival().do(self.problem, self.pop, return_indices=True)
+
+
+class DEPaper(Algorithm):
+
+    def __init__(self,
+                 CR,
+                 pop_size=30,
+                 tau=0.1,
+                 L=0.05,
+                 U=0.35,
+                 Fmin=0.0,
+                 Fmax=1.0,
+                 **kwargs):
+
+        super().__init__(**kwargs)
+
+        self.pop_size = pop_size
+        self.tau = tau
+        self.L = L
+        self.U = U
+        self.Fmin = Fmin
+        self.Fmax = Fmax
+        if CR is None:
+            raise ValueError("DEPaper needs a CR Value")
+        self.CR = CR
+        self.D = int(2/self.CR)
+
+
+    def _initialize_infill(self):
+        X = self._initialize_population()
+
+        self.pop = Population.new(X=X)
+
+        Fvec = np.random.uniform(
+            self.Fmin,
+            self.Fmax,
+            self.pop_size
+        )
+
+        self.pop.set("Fvec", Fvec) # Vetor de F auto-adaptativos
+
+        self.evaluator.eval(self.problem, self.pop)
+
+        return self.pop
+    
+
+    def _initialize_advance(self, infills=None, **kwargs):
+        self.pop = infills
+        self.evaluator.eval(self.problem, self.pop)
+
+
+    def _initialize_population(self):
+
+        pop = np.zeros((self.pop_size, self.D))
+
+        for i in range(self.pop_size):
+
+            pop[i] = self._initialize_individual()
+
+        return pop
+
+
+    def _initialize_individual(self, t_kmeans=20):
+
+        rng = np.random.default_rng(self.random_state)
+        n_samples = self.problem.dataset_data.shape[0]
+
+        # primeiro centróide aleatório escolhido dos dados
+        idx = rng.integers(0, n_samples)
+        centroids = [self.problem.dataset_data[idx]]
+
+        # escolher os centroides demais
+        for _ in range(1, self.problem.n_clusters):
+            # amostra t pontos aleatórios (sem replacement)
+            candidates_idx = rng.choice(n_samples, size=min(t_kmeans, n_samples), replace=False)
+            candidates = self.problem.dataset_data[candidates_idx]
+
+            # calcula distância mínima de cada candidato até centróides existentes
+            dists = []
+            for x in candidates:
+                min_dist = np.min([np.linalg.norm(x - c) for c in centroids])
+                dists.append(min_dist)
+
+            # pega o mais distante
+            best = candidates[np.argmax(dists)]
+            centroids.append(best)
+
+        return np.array(centroids).flatten()
+
+
+    def _adapt_F(self):
+
+        Fvec = self.pop.get("Fvec").copy()
+
+        mask = np.random.rand(len(Fvec)) < self.tau
+
+        Fvec[mask] = self.L + np.random.rand(np.sum(mask)) * (self.U - self.L)
+
+        self.pop.set("Fvec", Fvec)
+
+
+    def _mutation(self):
+
+        X = self.pop.get("X")
+        Fvec = self.pop.get("Fvec")
+
+        mutant = np.empty_like(X)
+
+        for i in range(self.pop_size):
+
+            idx = np.arange(self.pop_size)
+
+            idx = idx[idx != i]
+
+            r1, r2, r3 = np.random.choice(
+                idx,
+                3,
+                replace=False
+            )
+
+            mutant[i] = (
+                X[r1]
+                + Fvec[i] * (X[r2] - X[r3])
+            )
+
+        return mutant
+
+
+    def _crossover(self, mutant):
+
+        offspring = self.pop.get("X").copy()
+
+        for i in range(self.pop_size):
+
+            drand = np.random.randint(self.D)
+
+            for j in range(self.D):
+
+                if np.random.rand() <= self.CR or j == drand:
+
+                    offspring[i, j] = mutant[i, j]
+
+        return offspring
+    
+
+    def _kmeans_exploitation(self,population):
+        for i, chromosome in enumerate(population):
+            centroids = chromosome.reshape(self.problem.n_clusters,self.problem.dataset_data.shape[1])
+            # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+            km = KMeans(
+                init=centroids,
+                n_clusters=self.problem.n_clusters,
+                n_init=1,
+                max_iter=1,
+                tol=1e-4
+            )
+
+            km.fit(self.problem.dataset_data)
+            population[i] = km.cluster_centers_.flatten()
+        return np.array(population) 
+
+    
+    def _selection(self, temp_offspring):
+
+        off = Population.new(X=temp_offspring)
+        self.evaluator.eval(self.problem, off)
+
+        off_X = off.get("X").copy()
+        off_X = self._kmeans_exploitation(off_X)
+
+        off.set("X", off_X)
+        self.evaluator.eval(self.problem, off)
+
+        # Atualizo se o indivíduo da população temporária for melhor que a versão não mutada+crossover
+        X = self.pop.get("X")
+        F = self.pop.get("F")
+        X_off = off.get("X")
+        F_off = off.get("F")
+
+        F = np.array(F)
+        F_off = np.array(F_off)
+
+        replace = F_off[:, 0] < F[:, 0]
+        
+        X[replace] = X_off[replace]
+        F[replace] = F_off[replace]
+
+
+        # atualiza indivíduos e fitness
+        self.pop.set("X", X)
+        self.pop.set("F", F)
+
+
+    def _advance(self, infills=None, **kwargs):
+
+        self._adapt_F()
+
+        mutant = self._mutation()
+        temp_offspring = self._crossover(mutant)
+        self._selection(temp_offspring)
+
+        # reavaliar população atual
+        self.evaluator.eval(self.problem, self.pop)
 
 
 class ClusterProblem(Problem):
@@ -139,32 +338,6 @@ class ClusterProblem(Problem):
 
         out["F"] = fitness_vec
 
-
-class KMeansRandomSampling(Sampling):
-
-    def __init__(self, data, cluster_n):
-        super().__init__()
-        self.data = data
-        self.cluster_n = cluster_n
-
-    def _do(self, problem, n_samples, **kwargs):
-
-        x = []
-
-        for _ in range(n_samples):
-
-            km = KMeans(
-                init='random',
-                n_clusters=self.cluster_n,
-                n_init=1,
-                max_iter=1
-            )
-
-            km.fit(self.data)
-
-            x.append(km.cluster_centers_.flatten())
-
-        return np.asarray(x)
 
 
 def kmeans_explotation(population,data, cluster_n):
@@ -212,8 +385,8 @@ def RunPymooProblem(
         save_history=save_history
     )
     
-    # print(res.X)   # melhor cromossomo (12 valores)
-    # print(res.F)   # melhor SSE
+    print(res.X)   # melhor cromossomo (12 valores)
+    print(res.F)   # melhor SSE
     # print("history:", res.history)
     # print("len(history):", len(res.history) if res.history is not None else None)
     # if res.history:
@@ -224,7 +397,7 @@ def RunPymooProblem(
 def ESClusterizationProblems(
     seed: int | None,
     es_class,
-    max_it: int = 200,
+    max_it: int = 80,
 ):
     iris_problem = ClusterProblem(
         x=x_iris,
@@ -283,9 +456,8 @@ def ESClusterizationProblems(
 
 def DEClusterizationProblems(
     de_class,
-    max_it: int = 200,
+    max_it: int = 80,
     seed: int | None = None,
-    sampling: Sampling | None = None,
 ):
     #https://pymoo.org/algorithms/soo/de.html
     iris_problem = ClusterProblem(
@@ -300,77 +472,39 @@ def DEClusterizationProblems(
         x=x_wine,
         y=y_wine,
     )
-    if not sampling:
-        iris = RunPymooProblem(
-            algorithm=de_class(
-                variant="DE/best/1/bin",
-                pop_size=200,
-                CR=0.2,
-                F=0.3,
-            ),
-            problem=iris_problem,
-            termination=("n_iter", max_it),
-            seed=seed,
-        )
-        breast = RunPymooProblem(
-            algorithm=de_class(
-                variant="DE/best/1/bin",
-                pop_size=200,
-                CR=0.2,
-                F=0.3,
-            ),
-            problem=breast_problem,
-            termination=("n_iter", max_it),
-            seed=seed,
-        )
-        wine = RunPymooProblem(
-            algorithm=de_class(
-                variant="DE/best/1/bin",
-                pop_size=200,
-                CR=0.2,
-                F=0.3,
-            ),
-            problem=wine_problem,
-            termination=("n_iter", max_it),
-            seed=seed,
-        )
-    else:
-        iris = RunPymooProblem(
-            algorithm=de_class(
-                variant="DE/best/1/bin",
-                pop_size=200,
-                CR=0.2,
-                F=0.3,
-                sampling=sampling,
-            ),
-            problem=iris_problem,
-            termination=("n_iter", max_it),
-            seed=seed,
-        )
-        breast = RunPymooProblem(
-            algorithm=de_class(
-                variant="DE/best/1/bin",
-                pop_size=200,
-                CR=0.2,
-                F=0.3,
-                sampling=sampling,
-            ),
-            problem=breast_problem,
-            termination=("n_iter", max_it),
-            seed=seed,
-        )
-        wine = RunPymooProblem(
-            algorithm=de_class(
-                variant="DE/best/1/bin",
-                pop_size=200,
-                CR=0.2,
-                F=0.3,
-                sampling=sampling,
-            ),
-            problem=wine_problem,
-            termination=("n_iter", max_it),
-            seed=seed,
-        )
+    iris = RunPymooProblem(
+        algorithm=de_class(
+            variant="DE/best/1/bin",
+            pop_size=200,
+            CR=2.0/float((np.unique(y_iris).size * x_iris.shape[1])),
+            F=0.3,
+        ),
+        problem=iris_problem,
+        termination=("n_iter", max_it),
+        seed=seed,
+    )
+    breast = RunPymooProblem(
+        algorithm=de_class(
+            variant="DE/best/1/bin",
+            pop_size=200,
+            CR=2.0/float((np.unique(y_breast).size * x_breast.shape[1])),
+            F=0.3,
+        ),
+        problem=breast_problem,
+        termination=("n_iter", max_it),
+        seed=seed,
+    )
+    wine = RunPymooProblem(
+        algorithm=de_class(
+            variant="DE/best/1/bin",
+            pop_size=200,
+            CR=2.0/float((np.unique(y_wine).size * x_wine.shape[1])),
+            F=0.3,
+        ),
+        problem=wine_problem,
+        termination=("n_iter", max_it),
+        seed=seed,
+    )
 
     
     return FitnessHistory(
@@ -378,7 +512,6 @@ def DEClusterizationProblems(
         breast=breast.history,
         wine=wine.history,
     )
-
 
 
 def plot_results_table(fitness_dict):
@@ -393,7 +526,7 @@ def plot_results_table(fitness_dict):
 
             best_runs = []
             iter_runs = []
-            print(histories)
+            # print(histories)
             for fitness_history in histories:
 
                 dataset = getattr(fitness_history, dataset_name)
@@ -450,95 +583,20 @@ def plot_results_table(fitness_dict):
         best = float(row[2])
         mean = float(row[3])
 
-        if np.isclose(best, best_per_dataset[dataset][0]):
+        if best == best_per_dataset[dataset][0]:
             table[(i, 2)].get_text().set_fontweight("bold")
 
-        if np.isclose(mean, best_per_dataset[dataset][1]):
+        if mean == best_per_dataset[dataset][1]:
             table[(i, 3)].get_text().set_fontweight("bold")
 
     plt.tight_layout()
     plt.show()
 
 
-def DE_Kmeans_Clusterization(data, cluster_n, max_it, seed):
-    #https://scikit-opt.github.io/scikit-opt/#/en/README?id=_1-differential-evolution
-    DE_population = []
-    # best_fit_history = []
-
-    fitness_func = partial( # Reaproveito a função feita para o Pymoo
-        fitness,
-        data=data,
-        cluster_n=cluster_n
-    )
-
-    bf = None
-
-    # população inicial via KMeans
-    for i in range(DE_population_size):
-
-        km = KMeans(
-            init='random', #'k-means++', # Começa com centroídes distrubuídos baseados na inércia dos dados
-            n_clusters=cluster_n, # Inicia N vezes e acha o melhor, mas no nosso caso como estamos usando um híbrido, não importa
-            n_init=1,
-            max_iter=1,
-            random_state=seed+i
-        )
-
-        km.fit(data)
-
-        chromosome = km.cluster_centers_.flatten()
-
-        DE_population.append(chromosome)
-
-        fit = fitness_func(chromosome)
-
-        if bf is None or fit < bf:
-            bf = fit
-
-    # best_fit_history.append(bf)
-
-    lb = np.tile(data.min(axis=0), cluster_n)
-    ub = np.tile(data.max(axis=0), cluster_n)
-
-    de = DE(
-        func=fitness_func,
-        n_dim=cluster_n * data.shape[1],
-        size_pop=DE_population_size,
-        max_iter=1,
-        lb=lb,
-        ub=ub,
-        F=0.1,
-        prob_mut=0.03
-    )
-
-    # substitui população inicial
-    # https://scikit-opt.github.io/scikit-opt/#/en/more_ga
-    de.X = np.array(DE_population)
-
-    for _ in range(max_it):
-
-        best_x, best_y = de.run()
-
-        DE_population = de.X.copy()
-
-        kmeans_explotation(
-            DE_population,
-            data,
-            cluster_n
-        )
-
-        de.X = DE_population
-
-        bf = min(fitness_func(ind) for ind in DE_population)
-        # best_fit_history.append(bf)
-
-    print(best_x)
-    print(best_y)
-
-    return best_x,best_y
-
-
 def main():
+    np.random.seed(1000) # Para facilmente ser reproduzível
+    seeds = np.random.randint(0, 2**32 - 1, size=5)
+    # print(seeds)
     # fitness_es_dict = {}
     # fitness_es_dict["es"] = []
     # fitness_es_dict["es-elitism"] = []
@@ -559,37 +617,20 @@ def main():
     fitness_de_dict = {}
     fitness_de_dict["de"] = []
     fitness_de_dict["de-kmeans"] = []
-    fitness_de_dict["de-kmeans-sampling"] = []
-    for seed in np.random.randint(0, 2**32 - 1, size=1):
-        fitness_de = DEClusterizationProblems(seed=seed, de_class=DE)
-        fitness_de_kmeans = DEClusterizationProblems(seed=seed, de_class=CustomDE_Kmeans)
+    fitness_de_dict["de-paper"] = []
+    for i,seed in enumerate(seeds):
+        it_start_time = time()
+        print(f"Running Seed {i+1} : {seed}")
+        # fitness_de = DEClusterizationProblems(seed=seed, de_class=DE)
+        # fitness_de_kmeans = DEClusterizationProblems(seed=seed, de_class=CustomDE_Kmeans)
+        fitness_de_paper = DEClusterizationProblems(seed=seed,de_class=DEPaper)
 
-        fitness_de_dict["de"].append(fitness_de)
-        fitness_de_dict["de-kmeans"].append(fitness_de_kmeans)
+        # fitness_de_dict["de"].append(fitness_de)
+        # fitness_de_dict["de-kmeans"].append(fitness_de_kmeans)
+        fitness_de_dict["de-paper"].append(fitness_de_paper)
+        print(f"Finished Seed {i+1}: {seed}, Runtime : {time()-it_start_time:.2f}s")
     plot_results_table(fitness_de_dict)
     
-
-    # fit_history_KC = Kmeans_Clusterization()
-    # fit_history_GAKC = GA_Kmeans_Clusterization()
-
-    # plt.figure(figsize=(12, 4))
-
-    # plt.subplot(2, 1, 1)
-    # plt.plot(fit_history_KC)
-    # plt.title("K-Means")
-    # plt.xlabel("Iteração")
-    # plt.ylabel("SSE")
-    # plt.grid(True)
-
-    # plt.subplot(2, 1, 2)
-    # plt.plot(fit_history_GAKC)
-    # plt.title("GA + K-Means")
-    # plt.xlabel("Geração")
-    # plt.ylabel("SSE")
-    # plt.grid(True)
-
-    # plt.tight_layout()
-    # plt.show()
 
 
 if __name__ == "__main__":
